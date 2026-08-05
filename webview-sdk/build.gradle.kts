d8e654b2dcd7e8eab052510684b37a42700beaf8
mengyiaba-code <mengyiaba@gmail.com>
2026-08-05 09:48:03 +0700

    Add initial build configuration for webview-sdk

---
+++ webview-sdk/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+android {
+    // This acts as your SDK's unique identifier
+    namespace = "com.yourusername.webviewsdk"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create("release") {
+                from(components["release"])
+                // Change these to your actual GitHub username
+                groupId = "com.github.YourUsername"
+                artifactId = "webview-sdk"
+                version = "1.0.0"
+            }
+        }
+    }
+}
